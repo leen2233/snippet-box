8959f005b9493c7b5106be62cd46f5298178d82e
leen2233 <arslanrejepow223@gmail.com>
2025-09-25 14:06:52 +0800

    added validator

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,11 +5,10 @@ import (
   "net/http"
   "strconv"
   "fmt"
-  "strings"
-  "unicode/utf8"
 
   "github.com/julienschmidt/httprouter"
 	"snippetbox.leen2233.me/internal/models"
+	"snippetbox.leen2233.me/internal/validator"
 )
 
 
@@ -17,7 +16,7 @@ type snippetCreateForm struct {
   Title       string
   Content     string
   Expires     int
-  FieldErrors map[string]string
+  validator.Validator
 }
 
 
@@ -87,24 +86,14 @@ func (app *application) CreateSnippetPost(w http.ResponseWriter, r *http.Request
     Title:        r.PostForm.Get("title"),
     Content:      r.PostForm.Get("content"),
     Expires:      expires,
-    FieldErrors:  make(map[string]string),
   }
 
-  if strings.TrimSpace(form.Title) == "" {
-    form.FieldErrors["title"] = "This field cannot be null"
-  } else if utf8.RuneCountInString(form.Title) > 100 {
-    form.FieldErrors["title"] = "This field letter count can't exceed 100 characters'"
-  }
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
-  if strings.TrimSpace(form.Content) == "" {
-    form.FieldErrors["content"] = "This field cannot be null"
-  }
-
-  if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-    form.FieldErrors["expires"] = "This field should be one of 1, 7, 365"
-  }
-
-  if len(form.FieldErrors) > 0 {
+  if !form.Valid() {
     data := app.newTemplateData(r)
     data.Form = form
 
